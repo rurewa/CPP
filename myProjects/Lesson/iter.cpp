7db72d14ef276bff85f3b48a28d59ea09af0846d
Alex <rise.sp@gmail.com>
2019-11-04 07:11:16 +0300

    Update

---
+++ myProjects/Lesson/iter.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int x = 2, y = 5, z;
+
+int foo(int a, int b)
+{
+  int result = 1, i;
+  for (i = 0; i < b; ++i)
+    {
+      result *= a;
+    }
+  return result;
+}
+
+int main()
+{
+  z = foo(x, y);
+
+  cout << z << endl;
+}
