988f7d3da683997d01da2db5d84f81a310977bfb
rurewa <rise.sp@gmail.com>
2019-12-28 06:25:04 +0300

    Add testing knowledge of yhe multipication table

---
+++ myProjects/Testing_knowledge_of_the_multiplication_table/arithm.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstdlib> // Библиотека таймера
+#include<ctime>
+using namespace std;
+
+// Генерация случайного числа
+int randNums1() {
+  srand(time(0));
+  int result = 1 + (rand() % 10);
+  return result;
+}
+// Генерация псевдослучайного числа
+int randNums2(int min = 1, int max = 10) {
+  return min + rand() % (max - min);
+}
+
+int main() {
+  cout.setf(ios::boolalpha); // Для вывода слов true/false
+  char userSelection = 'y';
+  int num = 0; // Для ввода ответа пользователем
+  int check = 0; // Для сравнения
+  bool answer = false; // Для вывода результата
+
+  cout << "Testing knowledge of the multiplication table" << endl;
+  while (userSelection != 'n') {
+    cout << randNums1() << " x " << randNums2() << " ? " << endl;
+    check = randNums1() * randNums2();
+    cin >> num;
+    if (num == check) {
+      cout << !answer << endl;
+    }
+    else {
+      cout << answer << endl;
+    }
+    cout << "Press (y) to replay or (n) key to exit" << endl;
+    cin >> userSelection;
+  }
+
+  return 0;
+}
