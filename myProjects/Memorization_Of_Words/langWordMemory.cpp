c061036d226ff1f7f48e192afe05af14fa7cab9b
rurewa <rise.sp@gmail.com>
2020-03-07 12:16:03 +0300

    Update program Memorization_Of_Words

--- myProjects/Memorization_Of_Words/langWordMemory.cpp
+++ myProjects/Memorization_Of_Words/langWordMemory.cpp
@@ -8,20 +8,23 @@
 #include<ctime> // Преобразование времени в строку
 using namespace std;
 
+const int lenArray = 9; // Размер массива
+
 // Генерация случайного числа
 int randNums1(int result = 0) {
   srand(time(0));
-  return result = 0 + (rand() % 6);
+  return result = 0 + (rand() % lenArray);
 }
 
 int main() {
   string userInWord; // Для ввода слова от пользователя
-  char again = 'y'; // Для продолжения/выхода программы
-  string enWords[6] = {"meet", "let", "ride", "hold", "hear", "have"};
-  string rusWords[6] = {"встречаться", "означать", "кататься", "держать", "слышать", "иметь"};
+  char again =  'y'; // Для продолжения/завершения программы
+  string enWords[lenArray] = {"meet", "let", "ride", "hold", "hear", "have", "grow", "give", "get"};
+  string ruWords[lenArray] = {"встречаться", "означать", "кататься", "держать", "слышать", "иметь",
+                                                              "расти", "давать", "получать"};
   do {
     int numArray = randNums1();
-    cout << "Translate the word - " << rusWords[numArray] << endl;
+    cout << "Translate the word - " << ruWords[numArray] << endl;
     cin >> userInWord;
     if (enWords[numArray] == userInWord) {
       cout << "True!" << endl;
