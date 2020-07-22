38ea27196abb744ef67b7ed59bca0c8de3d963ff
Alex <rurewa@gmail.com>
2020-07-22 19:26:49 +0300

    Update calculator v2.3

--- myProjects/Calculator/v2_0/getMatematicalOperation.cpp
+++ myProjects/Calculator/v2_0/getMatematicalOperation.cpp
@@ -2,9 +2,21 @@
 
 // Принимаем знак оператора от пользователя
 char getMatematicalOperation() {
-    std::cout << "Please enter which operator: " << std::endl;
-    char op;
-    std::cin >> op;
-    // Тут должна быть защита от ввода некоректного символа
-    return op;
+    while (true) {
+        std::cout << "Enter one of the following: +, -, *, /: ";
+        char op;
+        std::cin >> op;
+        // Переменные типа char могут принимать любые символы из
+        // пользовательского ввода, поэтому нам не стоит беспокоиться по поводу
+        // возникновения неудачного извлечения
+        std::cin.ignore(32767, '\n'); // удаляем лишний балласт
+        // Выполняем проверку пользовательского ввода
+        if (op == '+' || op == '-' || op == '*' || op == '/') {
+            return op; // возвращаем обратно в caller (main)
+        }
+        else { // в противном случае, сообщаем пользователю что что-то пошло не так
+            std::cout << "Oops! That input is invalid. Please try again.\n";
+        }
+    }
+
 }
--- myProjects/Calculator/v2_0/getUserInput.cpp
+++ myProjects/Calculator/v2_0/getUserInput.cpp
@@ -2,8 +2,19 @@
 
 // Принимаем значение от пользователя
 double getUserInput() {
-    std::cout << "Please enter an integer: " << std::endl;
-    double value = 0;
-    std::cin >> value;
-    return value;
+    while (true) { // цикл продолжается до тех пор, пока пользователь не введёт корректное значение
+        std::cout << "Please enter a double valie: " << std::endl;
+        double value = 0;
+        std::cin >> value;
+        // Проверка на предыдущие извлечения
+        if (std::cin.fail()) { // Если предыдущее извлечение неудачно
+            std::cin.clear(); // Возвращаем cin в обычный режим
+            std::cin.ignore(32767, '\n'); // и удаляем значения предыдущего ввода из входного буфера
+            std::cout << "Oops, that input is invalid. Please try agin.\n";
+        }
+        else {
+            std::cin.ignore(32767, '\n'); // удаляем лишние значения
+            return value;
+        }
+    }
 }
