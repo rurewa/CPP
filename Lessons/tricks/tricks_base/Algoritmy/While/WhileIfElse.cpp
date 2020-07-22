131198cc84008a13382564f8eaa563a2ff746f1a
Alex <rurewa@gmail.com>
2020-07-22 14:01:00 +0300

    Added new lessons

---
+++ Lessons/tricks/tricks_base/Algoritmy/While/WhileIfElse.cpp
@@ -0,0 +1,35 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// Пример, который считает кол-во попыток ввода символа
+// V 1.0
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+#include <iostream>
+
+int main() {
+    using namespace std;
+    int count(0); // Считаем кол-во попыток
+    bool exitLoop(false); // Контроль завершения выполнения цикла
+    while (!exitLoop) {
+        cout << "Enter 'e' to exit this loop or any other key to continue: ";
+        char sm;
+        cin >> sm;
+
+        if (sm == 'e') {
+            exitLoop = true;
+        }
+        else {
+            ++count;
+            cout << "We've iterated " << count << " times\n";
+        }
+    }
+
+    return 0;
+}
+// Output:
+/*
+Enter 'e' to exit this loop or any other key to continue: w
+We've iterated 7 times
+Enter 'e' to exit this loop or any other key to continue: e
+*/
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// END FILE
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
