7aa43ec6c16cb6c3a5ee4e1c65da2a24f765fa41
rurewa <34348805+rurewa@users.noreply.github.com>
2020-08-12 08:46:55 +0300

    Update interface_sharing1.cpp

--- Lessons/tricks/tricks_base/Functions/interface_sharing.cpp/interface_sharing1.cpp
+++ Lessons/tricks/tricks_base/Functions/interface_sharing.cpp/interface_sharing1.cpp
@@ -4,49 +4,50 @@
 // v 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 #include <iostream>
-using namespace std;
 
-bool isEqual(int a, int b) { 
+bool isEqual(int a, int b);
+
+int userInput();
+
+void printResultEqual(int x, int y);
+
+void printResultNoEqual(int x, int y);
+
+void printResult(int x, int y);
+
+int main()
+{
+    std::cout << "Enter is first integer number: " << std::endl;
+    int firstNumber = userInput();
+    std::cout << "Enter is second integer number: " << std::endl;
+    int secondNumber = userInput();
+
+    printResult(firstNumber, secondNumber);
+
+    return 0;
+}
+
+bool isEqual(int a, int b) {
     return (a == b); // Возвращает true или false
 }
 
-int userFirstPrintInput() { // 1-е значение от пользователя
-    cout << "Enter is first number: " << endl;
+int userInput() { // 1-е значение от пользователя
     int x;
-    cin >> x;
-
+    std::cin >> x;
     return x;
 }
 
-int userSecondPrintInput() { // 2-е значение от пользователя
-    cout << "Enter is second number: " << endl;
-    int y;
-    cin >> y;
-
-    return y;
-}
-
 void printResultEqual(int x, int y) { // Вывод равенства
-    cout << x << " end " << y << " are equal " << endl;
+    std::cout << x << " end " << y << " are equal " <<std:: endl;
 }
 
 void printResultNoEqual(int x, int y) { // Вывод неравенства
-    cout << x << " end " << y << " are not equal " << endl;
+    std::cout << x << " end " << y << " are not equal " << std::endl;
 }
 
 void printResult(int x, int y) { // Вывод результата
     if (isEqual(x, y)) { printResultEqual(x, y); }
     else { printResultNoEqual(x, y); }
-}
-
-int main()
-{
-    int x, y;
-    x = userFirstPrintInput();
-    y = userSecondPrintInput();
-    printResult(x, y);
-
-    return 0;
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 // END FILE
