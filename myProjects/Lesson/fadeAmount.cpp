2a4fe9672948f850fa1f3efc4b5226b66141ab09
rurewa <34348805+rurewa@users.noreply.github.com>
2020-08-12 07:47:00 +0300

    Update fade amount v 1.0

--- myProjects/Lesson/fadeAmount.cpp
+++ myProjects/Lesson/fadeAmount.cpp
@@ -1,17 +1,23 @@
-#include<iostream>
-using namespace std;
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// Простой алгоритм "волна".
+// V 1.0
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+#include <iostream>
 
 int main() {
-  int i = 0;
-  int up = 0;
-  int step = 5;
-  while (i <= 100) {
-    ++i;
-    up += step;
-    if (up <= 0 || up >= 255) {
-      step = -step;
+    int i{};
+    int up{};
+    int step{5};
+    while (i <= 100) {
+        ++i;
+        up += step;
+        if (up <= 0 || up >= 255) {
+            step = -step;
+        }
+        std::cout << up << std::endl;
     }
-    cout << up << endl;
-  }
-  return 0;
+    return 0;
 }
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// END FILE
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
