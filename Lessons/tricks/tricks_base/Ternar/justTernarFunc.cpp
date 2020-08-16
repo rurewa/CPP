5ff5283bc6ec78b7248d8547fbb4965e59fc4f68
rurewa <rise.sp@gmail.com>
2020-08-16 18:47:11 +0300

    Update Just Ternar Function

--- Lessons/tricks/tricks_base/Ternar/justTernarFunc.cpp
+++ Lessons/tricks/tricks_base/Ternar/justTernarFunc.cpp
@@ -1,16 +1,22 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-// Простой пример использования тернарного оператора вместо if/else
+// Простые примеры использования тернарного оператора вместо if/else
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-#include<iostream>
+#include <iostream>
 
 int min(int a, int b) {
     return (a < b) ? a : b; // Использование тернарного оператора
 }
 
 int main() {
-    using namespace std;
-    cout << "Minimal number: " << min(3, 5) << endl;
+    // С функцией
+    std::cout << "Minimal number: " << min(3, 5) << std::endl;
+    // Без функции
+    int x{ 3 }, y{ 5 };
+    std::cout << ((x < y) ? x : y) << std::endl;
+    bool inBigClassroom = true;
+    const int classSize = inBigClassroom ? 3 : 5;
+    std::cout << classSize << std::endl;
     return 0;
 }
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
