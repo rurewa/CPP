860d32d81c3a9964f32a960a951ed40c5be0f364
rurewa <rurewa@gmail.com>
2020-12-04 13:41:17 +0300

    Update convert value programm

--- myProjects/ConvertValue/convertValue.cpp
+++ myProjects/ConvertValue/convertValue.cpp
@@ -1,33 +1,10 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 // Программа для преобразования cимвола в число и наоборот
-// v 1.0 beta
+// по таблице ANCii
+// v 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 #include <iostream>
-
-int chooseMenu() {
-    int inputChoose{};
-    std::cin >> inputChoose;
-    return inputChoose;
-}
-
-int userInputInt() {
-    int inputNumber{};
-    std::cin >> inputNumber;
-    return inputNumber;
-}
-
-char userInputChar() {
-    char inputChar{};
-    std::cin >> inputChar;
-    return inputChar;
-}
-
-void menu() {
-    using namespace std;
-    cout << "1. Convert char to int\n";
-    cout << "2. Convert int to char\n";
-    cout << "3. Exit\n";
-}
+#include "function.h"
 
 int main() {
     using namespace std;
