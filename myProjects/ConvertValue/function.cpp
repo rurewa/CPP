9e77a06ec83a5461836beba908397b5e7ef7cce4
rurewa <rurewa@gmail.com>
2020-12-04 13:47:47 +0300

    Update brakets to convert value

--- myProjects/ConvertValue/function.cpp
+++ myProjects/ConvertValue/function.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 #include "function.h"
 
-int chooseMenu()
-{
+int chooseMenu() {
     int inputChoose{};
     std::cin >> inputChoose;
     return inputChoose;
 }
 
-int userInputInt()
-{
+int userInputInt() {
     int inputNumber{};
     std::cin >> inputNumber;
     return inputNumber;
 }
 
-char userInputChar()
-{
+char userInputChar() {
     char inputChar{};
     std::cin >> inputChar;
     return inputChar;
 }
 
-void menu()
-{
+void menu() {
     using namespace std;
     cout << "1. Convert char to int\n";
     cout << "2. Convert int to char\n";
